59cf7572fa577730126ecd3534c26c2971e27068
Noah Soubliere <nsoubliere@lcs.on.ca>
2021-11-06 10:02:04 -0400

    Initial Commit

---
+++ HockeyCardsAssigment/HockeyCardsAssigmentApp.swift
@@ -0,0 +1,17 @@
+//
+//  HockeyCardsAssigmentApp.swift
+//  HockeyCardsAssigment
+//
+//  Created by Noah Alexandre Soubliere on 2021-11-06.
+//
+
+import SwiftUI
+
+@main
+struct HockeyCardsAssigmentApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
